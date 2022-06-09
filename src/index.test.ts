2f60f8c7efd4c7b70d9b3c3886719861f16a2972
anthony <anthonytran.dev@gmail.com>
2022-06-09 01:07:39 +0100

    Updated util functions

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,121 @@
-import { start } from "./index";
+import { generateStudentFacingTally, checkTally } from './index';
 
-describe("start", () => {
-  it('Should log "Hello, World!"', () => {
-    const consoleSpy = jest.spyOn(console, "log");
-    start();
-    expect(consoleSpy).toHaveBeenCalledWith("Hello, World!");
+const exampleDailyRota1: Array<string[]> = [
+  ['PH', 'PH', 'PH', 'BA', 'BA', 'TA', 'TA', 'BR', 'BR', 'BA', 'BA', 'HE', 'HE', 'PH', 'PH', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['HE', 'RE', 'RE', 'HE', 'HE', 'CH', 'CH', 'CH', 'CH', 'BR', 'BR', 'HE', 'HE', 'HE', 'HE', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['CH', 'CH', 'CH', 'CH', 'CH', 'PH', 'PH', 'PH', 'PH', 'BR', 'BR', 'PH', 'PH', 'BA', 'BA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['PH', 'PH', 'PH', 'BA', 'BA', 'PH', 'PH', 'BR', 'BR', 'PH', 'PH', 'PH', 'PH', 'BA', 'BA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'HE', 'HE', 'PH', 'PH', 'CH', 'CH', 'PH', 'PH', 'HE', 'HE', 'BR', 'BR', 'CH', 'CH', 'HE', 'HE', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'CH', 'CH', 'CH', 'CH', 'BA', 'BA', 'HE', 'HE', 'BR', 'BR', 'HE', 'HE', 'RE', 'RE', 'CH', 'CH', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'TR', 'TR', 'TR', 'TR', 'BA', 'BA', 'BA', 'BA', 'BR', 'BR', 'BA', 'BA', 'TR', 'TR', 'TR', 'TR', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'HE', 'HE', 'HE', 'HE', 'BA', 'BA', 'BA', 'BA', 'BR', 'BR', 'RE', 'RE', 'HE', 'HE', 'HE', 'HE', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'TR', 'TR', 'TR', 'TR', 'HE', 'HE', 'HE', 'HE', 'BR', 'BR', 'RE', 'RE', 'HE', 'HE', 'CH', 'CH', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'NA', 'NA', 'CH', 'CH', 'CH', 'CH', 'TR', 'TR', 'PH', 'PH', 'BR', 'BR', 'CH', 'CH', 'PH', 'PH', 'PH', 'PH', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'NA', 'NA', 'HE', 'HE', 'HE', 'HE', 'CH', 'CH', 'RE', 'RE', 'BR', 'BR', 'HE', 'HE', 'ME', 'ME', 'HE', 'HE', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'NA', 'NA', 'PH', 'PH', 'HE', 'HE', 'RE', 'RE', 'HE', 'HE', 'BR', 'BR', 'CH', 'CH', 'HE', 'HE', 'CH', 'CH', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'NA', 'NA', 'PH', 'PH', 'PH', 'PH', 'HE', 'HE', 'TR', 'TR', 'BR', 'BR', 'PH', 'PH', 'CH', 'CH', 'HE', 'HE', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'],
+  ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'CH', 'CH', 'CH', 'CH', 'PH', 'PH', 'BR', 'BR', 'PH', 'PH', 'TR', 'TR', 'PH', 'PH', 'BA', 'BA'],
+  ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'BA', 'BA', 'PH', 'PH', 'PH', 'PH', 'BR', 'BR', 'TR', 'TR', 'PH', 'PH', 'CH', 'CH', 'PH', 'PH'],
+  ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'PH', 'PH', 'CH', 'CH', 'CH', 'CH', 'PH', 'PH', 'BR', 'BR', 'PH', 'PH', 'CH', 'CH', 'PH', 'PH'],
+  ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'CH', 'CH', 'TR', 'TR', 'TR', 'TR', 'BR', 'BR', 'CH', 'CH', 'CH', 'CH', 'PH', 'PH', 'CH', 'CH'],
+];
+
+const studentFacingRoleTally = {
+  chat: [1, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 4, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1],
+  helpDesk: [1, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2, 3, 3, 4, 4, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0],
+  phone: [2, 2, 2, 3, 3, 3, 3, 2, 2, 3, 3, 3, 3, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+};
+
+describe('start', () => {
+  it('should return an array of numbers between 1 and 4', () => {
+    const studentFacing = generateStudentFacingTally(exampleDailyRota1);
+
+    expect(studentFacing).toEqual(studentFacingRoleTally);
+  });
+
+  it.each([
+    [
+      '0 people on "chat" role between 8:30am - 9:00am',
+      ['Not enough people for this "chat" role, requires at least 1 person at 8:30'],
+      { ...studentFacingRoleTally, chat: [0, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 4, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1] },
+    ],
+    [
+      '0 people on "phone" role between 8:30am - 9:00am',
+      ['Not enough people for this "phone" role, requires at least 1 person at 8:30'],
+      { ...studentFacingRoleTally, phone: [0, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 4, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1] },
+    ],
+    [
+      '0 people on "help desk" role between 8:30am - 9:00am',
+      ['Not enough people for this "help desk" role, requires at least 1 person at 8:30'],
+      { ...studentFacingRoleTally, helpDesk: [0, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 4, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1] },
+    ],
+    [
+      '0 people on all roles between 8:30am - 9:00am',
+      [
+        'Not enough people for this "chat" role, requires at least 1 person at 8:30',
+        'Not enough people for this "phone" role, requires at least 1 person at 8:30',
+        'Not enough people for this "help desk" role, requires at least 1 person at 8:30',
+      ],
+      {
+        chat: [0, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 4, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1],
+        helpDesk: [0, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2, 3, 3, 4, 4, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0],
+        phone: [0, 2, 2, 3, 3, 3, 3, 2, 2, 3, 3, 3, 3, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      },
+    ],
+    [
+      '1 person on role "chat" between 9am - 5pm',
+      ['Not enough people for this "chat" role, requires between 2-4 people at 14:30'],
+      { ...studentFacingRoleTally, chat: [1, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 1, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1] },
+    ],
+    [
+      '1 person on role "phone" between 9am - 5pm',
+      ['Not enough people for this "phone" role, requires between 2-4 people at 14:30'],
+      { ...studentFacingRoleTally, phone: [1, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 1, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1] },
+    ],
+    [
+      '1 person on role "help desk" between 9am - 5pm',
+      ['Not enough people for this "help desk" role, requires between 2-4 people at 14:30'],
+      { ...studentFacingRoleTally, helpDesk: [1, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 1, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1] },
+    ],
+    [
+      '5 people on role "chat" between 9am - 5pm',
+      ['Too many people for this "chat" role, requires between 2-4 people at 14:30'],
+      { ...studentFacingRoleTally, chat: [1, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 5, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1] },
+    ],
+    [
+      '5 people on role "phone" between 9am - 5pm',
+      ['Too many people for this "phone" role, requires between 2-4 people at 14:30'],
+      { ...studentFacingRoleTally, phone: [1, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 5, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1] },
+    ],
+    [
+      '5 people on role "help desk" between 9am - 5pm',
+      ['Too many people for this "help desk" role, requires between 2-4 people at 14:30'],
+      { ...studentFacingRoleTally, helpDesk: [1, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 5, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1] },
+    ],
+    [
+      'mulitple end times and roles 9am - 5pm',
+      [
+        'Too many people for this "chat" role, requires between 2-4 people at 8:30',
+        'Too many people for this "phone" role, requires between 2-4 people at 9:30',
+        'Not enough people for this "help desk" role, requires between 2-4 people at 9:00',
+      ],
+      {
+        chat: [1, 5, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 4, 4, 3, 3, 2, 2, 1, 1, 2, 2, 1, 1],
+        helpDesk: [1, 2, 1, 3, 3, 3, 3, 3, 3, 2, 2, 3, 3, 4, 4, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0],
+        phone: [2, 2, 2, 5, 3, 3, 3, 2, 2, 3, 3, 3, 3, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      },
+    ],
+  ])('errors when %s with expected error message', (_, errorMessages, tally) => {
+    const { errors } = checkTally(tally);
+    expect(errors).toEqual(errorMessages);
   });
 });
+
+// Solution
+// Input - the Shifts for the entire day
+// Output - rota types slots for student facing roles for entire day
+
+// 4 people number of timeslots
+// generates 1-6 - different role types
+// append zero <empty> slots
+//
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,129 @@
-export const start = () => console.log("Hello, World!");
+enum RoleTypes {
+  BR = 'BR', // 'BREAK',
+  PH = 'PH', // 'PHONES',
+  CH = 'CH', // 'CHAT',
+  HE = 'HE', // 'HELPDESK',
+  BA = 'BA', // 'BACK_OFFICE',
+  TR = 'TR', // 'TRIAGE',
+  TA = 'TR', // 'TRAINING',
+  ME = 'ME', // 'MEETINGS',
+  RE = 'RE', // 'RECEPTION_COVER',
+  NA = 'NA', //  no role assigned
+}
 
-start();
+interface StudentFacingTally {
+  chat: number[];
+  helpDesk: number[];
+  phone: number[];
+}
+
+interface Checker {
+  errors?: string[];
+}
+
+const endTimes = [
+  '8:30',
+  '9:00',
+  '9:30',
+  '10:00',
+  '10:30',
+  '11:00',
+  '11:30',
+  '12:00',
+  '12:30',
+  '13:00',
+  '13:30',
+  '14:00',
+  '14:30',
+  '15:00',
+  '15:30',
+  '16:00',
+  '16:30',
+  '17:00',
+  '17:30',
+  '18:00',
+  '18:30',
+  '19:00',
+  '19:30',
+  '20:00',
+  '20:30',
+  '21:00',
+];
+
+const evaluateRoles9am5pm = (type: string, count: number[]) =>
+  count
+    .slice(1, 17)
+    .map((slot: number, index: number) => {
+      let message = '';
+
+      if (slot < 2) {
+        message = `Not enough people for this "${type}" role, requires between 2-4 people at ${endTimes[index]}`;
+      }
+      if (slot > 4) {
+        message = `Too many people for this "${type}" role, requires between 2-4 people at ${endTimes[index]}`;
+      }
+
+      return message;
+    })
+    .filter(Boolean);
+
+const generateStudentFacingTally = (dailyRota: Array<string[]>): StudentFacingTally => {
+  const chat = Array(25).fill(0);
+  const helpDesk = Array(25).fill(0);
+  const phone = Array(25).fill(0);
+
+  for (let i = 0; i < dailyRota.length; i++) {
+    const shift = dailyRota[i];
+
+    for (let j = 0; j < shift.length; j++) {
+      if (shift[j] === RoleTypes.CH) {
+        chat[j] = chat[j] + 1;
+      } else if (shift[j] === RoleTypes.HE) {
+        helpDesk[j] = helpDesk[j] + 1;
+      } else if (shift[j] === RoleTypes.PH) {
+        phone[j] = phone[j] + 1;
+      }
+    }
+  }
+
+  return { chat, helpDesk, phone };
+};
+
+const checkTally = (tally: StudentFacingTally): Checker => {
+  const { chat, phone, helpDesk } = tally;
+  let errors = [];
+
+  if (chat[0] < 1) {
+    errors.push('Not enough people for this "chat" role, requires at least 1 person at 8:30');
+  }
+
+  if (phone[0] < 1) {
+    errors.push('Not enough people for this "phone" role, requires at least 1 person at 8:30');
+  }
+
+  if (helpDesk[0] < 1) {
+    errors.push('Not enough people for this "help desk" role, requires at least 1 person at 8:30');
+  }
+
+  console.log(!chat.slice(1, 17).every((slot: number) => slot >= 2 && slot <= 4));
+
+  if (!chat.slice(1, 17).every((slot: number) => slot >= 2 && slot <= 4)) {
+    const erroneousEndTimes = evaluateRoles9am5pm('chat', chat);
+    errors = [...errors, ...erroneousEndTimes];
+  }
+
+  if (!phone.slice(1, 17).every((slot: number) => slot >= 2 && slot <= 4)) {
+    const erroneousEndTimes = evaluateRoles9am5pm('phone', phone);
+    errors = [...errors, ...erroneousEndTimes];
+  }
+
+  if (!helpDesk.slice(1, 17).every((slot: number) => slot >= 2 && slot <= 4)) {
+    const erroneousEndTimes = evaluateRoles9am5pm('help desk', helpDesk);
+    errors = [...errors, ...erroneousEndTimes];
+  }
+
+  return { errors };
+};
+
+export { generateStudentFacingTally, checkTally };
+export type { RoleTypes };
